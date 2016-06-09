3dbac3183bdcc098b5add6dbf5d554a5cc1ad0aa
hnk1031 <v8gjyno9fl@gmail.com>
2016-06-09 16:09:17 +0900

    finish

--- app/src/main/java/com/example/hnk_1031/pittaritime/MainActivity.java
+++ app/src/main/java/com/example/hnk_1031/pittaritime/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.hnk_1031.pittaritime;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     Handler mHandler;
 
     int mTime;
+    int mDefaultTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,37 +34,60 @@ public class MainActivity extends AppCompatActivity {
         mResultTextView = (TextView)findViewById(R.id.textView2);
 
         mHandler = new Handler();
+
+        Intent intent = getIntent();
+        mDefaultTime = intent.getIntExtra("minutes", 0);
     }
 
     public void start(View v) {
-        mTime = 10;
+        if (mTimer == null) {
+            Toast.makeText(this, mDefaultTime + "秒当ててね", Toast.LENGTH_LONG).show();
 
-        mTimer = new Timer(false);
-        mTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                mHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        mTime--;
+            mTime = mDefaultTime;
 
-                        Log.d("timeの数字=",String.valueOf(mTime));
+            mTimer = new Timer();
+            mTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            mTime--;
 
+                            Log.d("timeの数字=", String.valueOf(mTime));
 
-                    }
-                });
+                        }
+                    });
 
-            }
-        }, 0, 1000 );
+                }
+            }, 0, 1000);
+        }
     }
 
     public void stop(View v){
-        if (mTime == 0){
-            mTimeTextView.setText(String.valueOf(mTime));
-            mResultTextView.setText("おめでとう！");
-        } else {
-            mTimeTextView.setText(String.valueOf(mTime));
-            mResultTextView.setText("残念！");
+        if (mTimer != null) {
+            mTimer.cancel();
+            mTimer = null;
+
+            if (mTime == 0) {
+                mTimeTextView.setText(String.valueOf(mTime));
+                mResultTextView.setText("おめでとう！");
+            } else {
+                mTimeTextView.setText(String.valueOf(mTime));
+                mResultTextView.setText("残念！");
+            }
         }
+
+    }
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if (mTimer !=null){
+            mTimer.cancel();
+            mTimer = null;
+        }
+
     }
 }
